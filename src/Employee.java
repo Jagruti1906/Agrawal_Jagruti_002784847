ed83952ca666c8263a91515dcfd89c7ebfe9807c
Jagruti1906 <jagruti1906@gmail.com>
2022-09-27 16:18:46 -0400

    Employee Class

---
+++ src/Employee.java
@@ -0,0 +1,17 @@
+
+import java.util.Date;
+
+public class Employee {
+    private String name;
+    private int employeeID;
+    private int age;
+    private String gender;
+    private Date startDate;
+    private int level;
+    private String teamInfo;
+    private String positionTitle;
+    private String phoneNumber;
+    private String email;
+    
+    
+}
