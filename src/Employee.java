b47a991054ae27a82512eaf443d119ba5d23bb6a
Jagruti1906 <jagruti1906@gmail.com>
2022-09-27 18:18:17 -0400

    Getters and setters

--- src/Employee.java
+++ src/Employee.java
@@ -25,4 +25,84 @@ public class Employee {
         this.phoneNumber = phoneNumber;
         this.email = email;
     }
+    
+    public String getName() {
+        return name;
+    }
+    
+    public int getEmployeeId() {
+        return employeeId;
+    }
+    
+    public int getAge() {
+        return age;
+    }
+    
+    public String getGender() {
+        return gender;
+    }
+    
+    public Date getStartDate() {
+        return startDate;
+    }
+    
+    public int getLevel() {
+        return level;
+    }
+    
+    public String getTeamInfo() {
+        return teamInfo;
+    }
+    
+    public String getPositionTitle() {
+        return positionTitle;
+    }
+    
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+    
+    public String getEmail() {
+        return email;
+    }
+    
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public void setEmployeeId(int employeeId) {
+        this.employeeId = employeeId;
+    }
+    
+    public void setAge(int age) {
+        this.age = age;
+    }
+    
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+    
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+    
+    public void setLevel(int level) {
+        this.level = level;
+    }
+    
+    public void setTeamInfo(String teamInfo) {
+        this.teamInfo = teamInfo;
+    }
+    
+    public void setPositionTitle(String positionTitle) {
+        this.positionTitle = positionTitle;
+    }
+    
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+    
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
